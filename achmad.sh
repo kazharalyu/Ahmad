e8fed87dce81314547380f802ec0e230f7c8f4af
KazharAlyu <85399207+kazharalyu@users.noreply.github.com>
2021-08-20 13:53:46 +0700

    Update achmad.sh

--- achmad.sh
+++ achmad.sh
@@ -3,7 +3,7 @@ clear
 echo "****************************"
 
 echo "____________________________"
-echo "Make By : Ahmad Guntur Alfandi :V"
+echo "PEMBUAT DAFA/MR-D47 ID,KONTAK 089528100123"
 echo "____________________________"
 echo "****************************"
 figlet Telp | lolcat
@@ -14,14 +14,14 @@ echo '
 '
 echo "::::::::::::::::::::::::::::::"
 echo
-read -p "Phone Numbers : " pil
+read -p "masukan pilihan kalian lurd : " pil
 if [[ $pil == 1 ]]; then
 read -p "Masukan No Target contoh:89528100123  : " nomor
 link="https://id.jagreward.com/member/verify-mobile/$nomor"
 curl -s $link
 else
-echo "Thanks For Use Tools ITs"
-echo "Welcome Back Again :)"
+echo "thanks ya lurd udah pakai tools MR-D47ID"
+echo "kontak admin?089528100123"
 exit
 fi
 echo
