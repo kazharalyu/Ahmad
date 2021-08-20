bd6d216e9a02b22dc9484c0af0e03832551971c5
KazharAlyu <85399207+kazharalyu@users.noreply.github.com>
2021-08-20 13:51:51 +0700

    Update achmad.sh

--- achmad.sh
+++ achmad.sh
@@ -1,30 +1,27 @@
-#script pertamax
+#!bin/bash
+clear
+echo "****************************"
 
-#Kode Warna
-MERAH="\033[31;1m"
-HIJAU="\033[32;1m"
-DELETE="\033[0m"
-
-
-#menampilkan menu
-
-echo -e  "${MERAH}____________________________________________________________"
-echo "Youtube : Ahmad Guntur Al"
-echo "Owner   : Ahmad Guntur Alfandi"
-echo "Co Owner: Brother Kasyifil Aziz"
-echo "admin : Captain Azka Permana"
-echo "Veteran : Muhammad El Faruq"
-echo "Members : Alisyah, Shofietunisa, Inunk"
-echo "____________________________________________________________"
-
-echo -e "${HIJAU}1. Menu Pertamax
-echo "2. Menu Keduax"
-echo "3. Menu Ketigax${HAPUS}"
-
-read -p "Silahkan Pilih Menu: " pil
-if [[ $pil = "1" ]] ;then
-echo "Kamu Memilih Menu Pertamax"
-elif [[ $pil = "2" ]]; then
-echo "kamu memilih menu keduax"
-elif [[ $pil = "3" ]]; then
-echo "kamu memilih menu ketigax
+echo "____________________________"
+echo "Make By : Ahmad Guntur Alfandi :V"
+echo "____________________________"
+echo "****************************"
+figlet Telp | lolcat
+echo "::::::::::::::::::::::::::::::"
+echo '
+[1], telepon spam
+[2], keluar &kontak admin
+'
+echo "::::::::::::::::::::::::::::::"
+echo
+read -p "Phone Numbers : " pil
+if [[ $pil == 1 ]]; then
+read -p "Masukan No Target contoh:89528100123  : " nomor
+link="https://id.jagreward.com/member/verify-mobile/$nomor"
+curl -s $link
+else
+echo "Thanks For Use Tools ITs"
+echo "Welcome Back Again :)"
+exit
+fi
+echo
